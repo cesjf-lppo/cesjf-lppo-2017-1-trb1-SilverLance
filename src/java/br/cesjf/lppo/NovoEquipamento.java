8b9d6983af794ff95dad959e5c9ddba692d197c6
AdrianoXavier <aj-dx@hotmail.com>
2017-04-02 11:59:13 -0300

    Criando NovoEquipamentoServlet.

---
+++ src/java/br/cesjf/lppo/NovoEquipamento.java
@@ -0,0 +1,74 @@
+package br.cesjf.lppo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Adriano
+ */
+@WebServlet(name = "NovoEquipamento", urlPatterns = {"/novo.html"})
+public class NovoEquipamento extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/novoEquipamento.jsp").forward(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String serie = request.getParameter("serie");
+        String local = request.getParameter("local");
+        String descricao = request.getParameter("descricao");
+        int estado = Integer.parseInt(request.getParameter("estado"));
+
+        Logger.getLogger(NovoEquipamento.class.getName()).log(Level.INFO, "POST: " + serie + "" + local + "" + descricao + "" + estado);
+
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            String url = "jdbc:derby://localhost:1527/lppo-2017-1";
+            Connection conexao = DriverManager.getConnection(url, "usuario", "senha");
+            System.out.println("Conexao aberta com sucesso!");
+
+            String sql = "INSERT INTO equipamento(serie, local, descricao, estado) VALUES('"
+                    + serie + "', "
+                    + local + "', "
+                    + descricao + "', "
+                    + estado + ")";
+
+            Statement operacao = conexao.createStatement();
+            operacao.executeUpdate(sql);
+            System.out.println("Registro inserido!");
+
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Driver indisponivel!");
+            Logger.getLogger(NovoEquipamento.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            System.err.println("Problema ao acessar o banco!");
+            Logger.getLogger(NovoEquipamento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.sendRedirect("lista.html");
+    }
+
+}
